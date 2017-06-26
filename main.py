173ff378b66eae611102d84609cffec36852554c
robisen1 <robisen@gmail.com>
2017-06-25 23:35:56 -0700

    minor updates

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+'''
+Simple UI for the BS Demo for RD group
+RS
+'''
 from kivy.app import App
+from kivy.uix.label import Label
+from kivy.clock import Clock
+import random
+import time
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty
 
+from kivy.factory import Factory
+
+#class GetRuningTime(Label):
+#    pass
+class BSdemoRoot(BoxLayout):
+    pass
+
+#class my widget
+class BSdemoForm(BoxLayout):
+    bsdemo_currenttime = StringProperty()
+    #bsdemo_currenttime = "12"
+    def __init__(self, **kwargs):
+        super(BSdemoForm, self).__init__(**kwargs)
+        self.bsdemo_currenttime = str(random.randint(1, 100))
+
+    def get_apptime(self):
+        self.bsdemo_currenttime = time.asctime()
 
 class BSKivyApp(App):
     pass
---
+++ readme.md
@@ -0,0 +1,12 @@
+read me
+
+Simple GUI for Blind Signals demo app.
+
+For the demo we only need to keep track of a few devices so we will only show
+Devices we expect for the Demo.
+
+ToDo:
+
+Update to dynamic labels
+Create update methods
+Create stubbed services to be wired in ZMQ or MQTT services
